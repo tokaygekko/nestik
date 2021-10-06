c153e94f9b9c32f69fddab70ac6fa5a04343c3a6
Zakovryashin Vadim <zakovryashin.v@gmail.com>
2021-10-06 22:04:02 +0300

    Add postgres, pgadmin with docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ProductsModule } from './products/products.module'
 import { CatsController } from './cats/cats.controller'
+import { ConfigModule } from '@nestjs/config'
+import * as Joi from '@hapi/joi'
+import { DatabaseModule } from './database/database.module'
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
